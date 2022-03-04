7c88cb43d712e227fd052db2d321674a84ff053a
unknown <d.drljepan816@gmail.com>
2022-03-04 20:05:03 +0100

    Maze

--- maze/script.js
+++ maze/script.js
@@ -1,4 +1,8 @@
-const { Engine, Render, World, Runner, Bodies } = Matter;
+const { Engine, Render, World, Runner, Bodies, MouseConstraint, Mouse } =
+  Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,15 +10,55 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600,
+    wireframes: false,
+    width: width,
+    height: height,
   },
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true,
-});
-World.add(world, shape);
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
+
+//Walls
+const walls = [
+  Bodies.rectangle(400, 0, 800, 50, {
+    isStatic: true,
+    render: { fillStyle: "gray" },
+  }),
+  Bodies.rectangle(400, 600, 800, 50, {
+    isStatic: true,
+    render: { fillStyle: "gray" },
+  }),
+  Bodies.rectangle(0, 300, 50, 600, {
+    isStatic: true,
+    render: { fillStyle: "gray" },
+  }),
+  Bodies.rectangle(800, 300, 50, 600, {
+    isStatic: true,
+    render: { fillStyle: "gray" },
+  }),
+];
+World.add(world, walls);
+
+//rand shapes
+
+for (let i = 0; i < 24; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  } else {
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 35)
+    );
+  }
+}

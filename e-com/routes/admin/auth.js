5e6afa4ad47ef1105bda78e29504399128e96a46
FSR1624 <d.drljepan816@gmail.com>
2022-03-17 21:03:19 +0100

    e-com_products_edit

--- e-com/routes/admin/auth.js
+++ e-com/routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
 
+const { handleErrors } = require("./middlewares");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -21,20 +21,15 @@ router.get("/signup", function (req, res) {
 router.post(
   "/signup",
   [requireEmail, requirePassword, requirePasswordConfirm],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-      // console.log(errors);
-    }
-
-    const { email, password, passwordConfirm } = req.body;
+    const { email, password } = req.body;
     //create a user into repository
     const user = await usersRepo.create({ email, password });
     //store id of that user inside the users cookie
     req.session.userId = user.id;
 
-    res.send(`Acc created`);
+    res.redirect("/admin/products");
   }
 );
 
@@ -50,17 +45,13 @@ router.get("/signin", (req, res) => {
 router.post(
   "/signin",
   [requireEmailExist, requireValidePasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
-
     const { email } = req.body;
     const user = await usersRepo.getOneBy({ email });
 
     req.session.userId = user.id;
-    res.send("You are signed in!");
+    res.redirect("/admin/products");
   }
 );
 
---
+++ e-com/views/admin/products/edit.js
@@ -0,0 +1,17 @@
+const layout = require("../layout");
+const { getError } = require("../helpers");
+
+module.exports = ({ product }) => {
+  return layout({
+    content: `
+    <form method="POST">
+    <input name="title" value="${product.title}" />
+    
+    <input name="price" value="${product.price}" />
+    
+    <input name="image" type="file" /> 
+    <button>Submit</button>
+  </form>
+      `,
+  });
+};
